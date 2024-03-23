4ef7a49f2020de88d04826955f6de4f86a896596
JhonAngulo <jhonmanuelm89@gmail.com>
2024-03-23 13:00:23 -0500

    20/03/2024

---
+++ 06_booleans.py
@@ -0,0 +1,10 @@
+is_single = True
+print(type(is_single))
+is_single = False
+print(is_single)
+
+print("True", not True)
+print("False", not False)
+
+is_single = not is_single
+print(is_single)
---
+++ 07_transformation.py
@@ -0,0 +1,18 @@
+name = "Jhon"
+print(type(name))
+name = 12
+print(type(name))
+
+print("Jhon" + "Angulo")
+print(10 + 20)
+# print("Jhon" + 10)  #TypeError: can only concatenate str (not "int") to str
+
+print("Jhon" + "12")
+
+age = 12
+print("Mi edad es " + str(age))
+
+age = input("Cuantos años tienes => ")
+print(type(age))
+
+print(f'Tu edad en 10 años será {int(age) + 10}')
